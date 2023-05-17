760dfa3dcbb81d6b357cb1544a2fd4f9744cb1bb
den2131123 <43034255+den2131123@users.noreply.github.com>
2023-05-17 21:01:56 +0300

    Add files via upload

---
+++ Js/js/lesson1.js
@@ -0,0 +1,16 @@
+let title = 'Проект'
+let screens = 'Простые, Сложные , Aдаптивные'
+let screenPrice = 5000
+let rollback = 100
+let fullPrice = 100000
+let adaptive = true
+
+
+console.log(typeof(title));
+console.log(typeof(fullPrice));
+console.log(screens.length);
+console.log(screenPrice + ' Рублей');
+console.log(fullPrice + ' Рублей');
+console.log(screens.toLowerCase());
+console.log(screens.split());
+console.log(fullPrice * (rollback/100));
